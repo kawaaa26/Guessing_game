30e53814d4c3d38be812faabcd1692a6b6cd4e29
kawaaa26 <unic.almj@yahoo.com>
2019-12-09 20:35:06 +0900

    :sparkles: secret number implemented, yet it's exposed.
    Closes #3

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("sup man, I let you guess the number.");
 
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
     println!("Input your guess.");
 
     let mut guess = String::new();
